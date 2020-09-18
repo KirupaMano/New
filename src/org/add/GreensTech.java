9172bcc049b04f455f005e5568e25114e7f52fdc
KirupaMano <skpm56@gmail.com>
2020-09-18 23:11:42 +0530

    Greens Address code added

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensTech {
+	public void greensOmr() {
+			System.out.println("OMR: Plot No.19,Balamurugan Garden,OMR Road,Kancheepuram District,Okkiam, Thoraipakkam,Jain college bus stop.");
+		}
+	public void greensAdayar() {
+			System.out.println("Adayar : No.11, First Street, padmanabha Nagar, Adyar,Chennai-600 020.");
+		}
+	public void greensTambaram() {
+			System.out.println("Tambaram: No.1, Apparao Colony, Tambaram, Sanatorium, Near HP Petrol Bunk, Chennai - 600047");
+		}
+	public void greensVelacherry() {
+			System.out.println("Velacherry: No.28, Nagendra Nagar, Opposite Phoenix Market City, Velachery, Chennai - 600042.");	
+			}
+	public void greensAnnaNagar() {
+			System.out.println("AnnaNagar: JK Pritzker, SDV Arcade, 4th floor, AB-5, 2nd Ave, Anna Nagar, Chennai - 600040.");	
+		}
+		public static void main(String[] args) {
+			GreensTech grn = new GreensTech();
+			grn.greensOmr();
+			grn.greensAdayar();
+			grn.greensTambaram();
+			grn.greensVelacherry();
+			grn.greensAnnaNagar();
+		}
+}
